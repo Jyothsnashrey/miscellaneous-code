02376ae6281dc53edfc87156311ad08530422e4e
Jyothsnashrey <jyothsna.shrey@gmail.com>
2024-01-30 12:26:57 -0600

    added terraform code

--- prometheus/ansible.sh
+++ prometheus/ansible.sh
@@ -1,6 +1,6 @@
 ssh_username=$(aws ssm get-parameter --name "ssh.username" --with-decryption --query 'Parameter.Value' --output text)
 ssh_password=$(aws ssm get-parameter --name "ssh.password" --with-decryption --query 'Parameter.Value' --output text)
 
-ansible-playbook -i 172.31.38.196, prometheus.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
+ansible-playbook -i 172.31.42.138, prometheus.yml -e ansible_user=$ssh_username -e ansible_password=$ssh_password
 
 
