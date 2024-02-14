3ddd4cc9e00b58ceeb2abfc6d236edda1e1872b1
Jyothsnashrey <jyothsna.shrey@gmail.com>
2024-02-14 16:54:23 -0600

    Move elastic search and prometheus to common code

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "tools" {
-  default={
+  default = {
+
     sonarqube = {
       instance_type = "t3.large"
       port          = 9000
-      priority = 100   # Load balancer Priority
+      priority      = 100
+    }
 
+    elasticsearch = {
+      instance_type = "m6in.large"
+      port          = 80
+      priority      = 101
+    }
+
+    prometheus = {
+      instance_type = "t3.small"
+      port          = 9090
+      priority      = 102
     }
   }
-  elasticsearch = {
-    instance_type = "m6in.large"
-    port          = 80
-    priority = 101   # Load balancer Priority
+}
 
-  }
-  prometheus = {
-    instance_type = "t3.small"
-    port          = 9090
-    priority      = 102
-  }
-
-
-  }
